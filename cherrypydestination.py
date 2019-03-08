e2e316a7c70f2d2fc5b53a057d1581121e3ab35a
Athithiyan Packirisamy <38016187+Adhithya7@users.noreply.github.com>
2019-03-08 12:06:11 +0530

    Add files via upload

--- cherrypydestination.py
+++ cherrypydestination.py
@@ -12,48 +12,19 @@ cherrypy.config.update({
     })
 
 @cherrypy.expose
-class cind_backup(object):
-    def GET(self):
+class service(object):
+    def GET(self,sv,ln):
         listex = []
-        f = open('/seclog/log/cinder/backup.log','r')
+        path = '/seclog/log/'+sv+'/'+ln
+        f = open(path,'r')
         for line in f:
             listex.append(line)
         return listex
 
-@cherrypy.expose
-class cind_api(object):
-    def GET(self):
-        listex = []
-        f = open('/seclog/log/cinder/api.log','r')
-        for line in f:
-            listex.append(line)
-        return listex
-
-@cherrypy.expose
-class cind_scheduler(object):
-    def GET(self):
-        listex = []
-        f = open('/seclog/log/cinder/scheduler.log','r')
-        for line in f:
-            listex.append(line)
-        return listex
-
-@cherrypy.expose
-class cind_volume(object):
-    def GET(self):
-        listex = []
-        f = open('/seclog/log/cinder/volume.log','r')
-        for line in f:
-            listex.append(line)
-        return listex
-
-log = ['cind_backup','cind_api','cind_scheduler','cind_volume','glan_api','glan_registry','keystone','nova_api','nova_manage','nova_novncproxy','nova_api-os-compute','nova_placement-api','nova_xvpvncproxy','nova_consoleauth','nova_console','nova_scheduler','nova_conductor','neu_dhcp','neu_metering','neu_server','neu_l3','neu_openvswitch','neu_metadata','neu_ovs-cleanup']
-
-for w in log:
-	cherrypy.tree.mount(cinder(), '/'+w,
-	    {'/':
-	         {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-	    }
-	)
+cherrypy.tree.mount(service(), '/service',
+    {'/':
+         {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
 
 cherrypy.engine.start()
--- cherrypyrelay.py
+++ cherrypyrelay.py
@@ -4,7 +4,7 @@ import string
 import requests
 
 cherrypy.config.update({
-    'server.socket_host':'192.168.1.30',
+    'server.socket_host':'192.168.1.30',#Edit Ip address
     'server.socket_port':8080,
     'server.ssl_module':'builtin',
     'server.ssl_certificate':'/home/smrelay/InterCA.pem',
@@ -12,20 +12,17 @@ cherrypy.config.update({
     })
 
 @cherrypy.expose
-class cinder(object):
-    def GET(self,host_name,log_name):
+class service(object):
+    def GET(self,host,sv,log_name):
         s=requests.Session()
         log_name=log_namein
-        r = s.get('https://'+host_name+':8000/'+log_name)
+        r = s.get('https://'+host+':8000/service',sv,log_name)
         return r.text
 
-log = ['cind_backup','cind_api','cind_scheduler','cind_volume','glan_api','glan_registry','keystone','nova_api','nova_manage','nova_novncproxy','nova_api-os-compute','nova_placement-api','nova_xvpvncproxy','nova_consoleauth','nova_console','nova_scheduler','nova_conductor','neu_dhcp','neu_metering','neu_server','neu_l3','neu_openvswitch','neu_metadata','neu_ovs-cleanup']
-
-for w in log:
-	cherrypy.tree.mount(cinder(), '/'+w,
-	    {'/':
-	         {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-	    }
-	)
+cherrypy.tree.mount(service(), '/service',
+    {'/':
+         {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
 
 cherrypy.engine.start()
